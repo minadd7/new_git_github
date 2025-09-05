7c870eaf5c46ea668a281a33382cd48c6ec867e2
chosun29 <sgmin.cho@mail.utoronto.ca>
2025-09-05 11:37:47 -0400

    first project

---
+++ basic_syntax/1.basic_command.sh
@@ -0,0 +1,8 @@
+# git add . 은 모든 변경(추가)사항을 staging area 로 이동하는 명령어입니다.
+git add .
+
+# git commit 은 staging area 의 변경사항을 확정짓고 commit ID 생성하는 명령어입니다.
+git commit -m "message"
+
+# 원격 repo 에 main 브렌치 변경사항(commit ID)을 업로드하는 명령어입니다.
+git push origin main
